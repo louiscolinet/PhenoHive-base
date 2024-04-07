5ba43f0278a4f1a6a57fa897546b4f16a5812325
Oldgram <goffindy@gmail.com>
2024-04-07 11:10:02 +0200

    Changed default logger level

--- main.py
+++ main.py
@@ -141,7 +141,7 @@ if __name__ == "__main__":
     # Parse arguments
     parser = argparse.ArgumentParser(description='Définition du niveau de log')
     parser.add_argument('-l', '--logger', type=str, help='Niveau de log (DEBUG, INFO, WARNING, ERROR,'
-                                                         'CRITICAL. Défaut = INFO', default='INFO')
+                                                         'CRITICAL. Défaut = DEBUG', default='DEBUG')
     args = parser.parse_args()
 
     # Setup logger
