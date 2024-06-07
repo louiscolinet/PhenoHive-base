343dea1fab5b9127d5b2ffd44be39d40db609608
Dylan Goffinet <goffindy@gmail.com>
2024-06-07 22:33:23 +0200

    Updated dependencies

--- setup_alpine.sh
+++ setup_alpine.sh
@@ -3,34 +3,53 @@
 # This script installs the necessary packages and enables the SPI interface on the Raspberry Pi
 # It also sets up the PhenoHive service to run on boot
 # It is intended to be run on a fresh install of Alpine Linux
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+GREEN='\033[0;32m'
+NC='\033[0m'
 
 # Check if the script is being run as root
 if [ "$EUID" -ne 0 ]
-  then echo "SETUP - Please run as root: sudo ./setup.sh"
+  then echo "${RED}Please run as root: sudo ./setup.sh"
   exit
 fi
 
 # Check if the script is being run on a Alpine-based system
 if [ ! -f /etc/alpine-release ]; then
-    echo "SETUP - This script is intended to run on a Alpine-based system."
+    echo "${RED}This script is intended to run on a Alpine-based system."
     exit
+fi
+
+# Check if 'git' is installed, if not, install it
+if ! command -v git &> /dev/null
+then
+    echo "${RED}git could not be found. Installing git..."
+    apk add git &> /dev/null
+fi
+
+# Check if 'python3' is installed, if not, install it
+if ! command -v python3 &> /dev/null
+then
+    echo "${YELLOW}python3 could not be found. Installing python3..."
+    apk add python3 &> /dev/null
 fi
 
 # Check if 'pip' is installed, if not, install it
 if ! command -v pip &> /dev/null
 then
-    echo "SETUP - pip could not be found. Installing pip..."
-    apk add python3 py3-pip python3-dev
+    echo "${YELLOW}pip could not be found. Installing pip..."
+    apk add py3-pip python3-dev
     pip3 install --upgrade pip setuptools wheel
 fi
 
-echo "SETUP - Installing necessary packages..."
+echo "${YELLOW}Installing necessary packages..."
 # Install the necessary packages
-apk update
+apk update &> /dev/null
 # Install gcc and related packages
-apk add gcc build-base build-essential libatlas-base-dev spi-tools musl-dev python-smbus python-pil
+apk add gcc build-base alpine-sdk py3-smbus spi-tools musl-dev py3-pillow
+# apk add gcc build-base build-essential libatlas-base-dev spi-tools musl-dev python-smbus python-pil
 pip3 install numpy
-pip3 install opencv
+pip3 install opencv-python
 pip3 install scipy
 pip3 install scikit-image
 pip3 install pandas
