3dc8e0086a3d5b0ce4d8f54bff8475a326bb73a1
Oldgram <goffindy@gmail.com>
2024-07-21 01:54:03 +0200

    Signal in the config file if the setup was done and when was it complete

--- tools/setup.sh
+++ tools/setup.sh
@@ -117,5 +117,10 @@ enable_spi
 # Setup PhenoHive as a service so PhenoHive/main.py is run on boot
 setup_service
 
+# Modify the config file to indicate that the setup has been completed
+sed -i 's/setup_complete = False/setup_complete = True/' $CONFIG_FILE
+sed -i 's/setup_date = .*/setup_date = "'"$(date +'%Y-%m-%d %H:%M:%S')"'"/' $CONFIG_FILE
+sed -i 's/absolute_path = .*/absolute_path = "'"$(pwd)"'"/' $CONFIG_FILE
+
 # Setup complete, reboot the Raspberry Pi
 echo -e "${INFO}Setup complete. A reboot is required before running the service.${WHITE}"
